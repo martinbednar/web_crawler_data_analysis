2013f852e6bde8952077806d5550eb55d57fcc4d
martinbednar <martin.bedn20@gmail.com>
2021-11-18 10:34:04 +0100

    Add "endpoints_calls_count" analysis.

--- start_analysis.py
+++ start_analysis.py
@@ -1,9 +1,20 @@
 import sqlite3
 import csv
 
+
+def export_results(results, output_file_path, csv_header):
+    with open(output_file_path, mode='w', newline='') as output_file:
+        csv_writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(csv_header)
+        for row in results:
+            csv_writer.writerow(row)
+
+
 def webpage_calls_count(cur, cur_p):
-    cur.execute("SELECT top_level_url, COUNT(*) as calls_count FROM javascript GROUP BY top_level_url ORDER BY calls_count DESC")
-    cur_p.execute("SELECT top_level_url, COUNT(*) as calls_count FROM javascript GROUP BY top_level_url ORDER BY calls_count DESC")
+    sql_query = "SELECT top_level_url, COUNT(*) as calls_count FROM javascript GROUP BY top_level_url ORDER BY calls_count DESC"
+    
+    cur.execute(sql_query)
+    cur_p.execute(sql_query)
 
     webs_calls_count = cur.fetchall()
     webs_calls_count_p = cur_p.fetchall()
@@ -17,24 +28,54 @@ def webpage_calls_count(cur, cur_p):
     for web_calls_count in webs_calls_count_p:
         if web_calls_count[0] in webs_calls_count_compare:
             webs_calls_count_compare[web_calls_count[0]].append(web_calls_count[1])
-
-    with open('webpage_calls_count.csv', mode='w', newline='') as output_file:
-        fieldnames = ['website', 'casual', 'privacy', 'difference']
-        csv_writer = csv.DictWriter(output_file, fieldnames=fieldnames, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writeheader()
-        for key, value in webs_calls_count_compare.items():
+    
+    results = []
+    
+    for key, value in webs_calls_count_compare.items():
             if len(value) == 2:
-                # We have data for both - casual and privacy crawling too
-                csv_writer.writerow({'website': key, 'casual': value[0], 'privacy': value[1], 'difference': value[0] - value[1]})
+                # We have data for both - casual and privacy crawling too.
+                results.append([key, value[0], value[1], value[0] - value[1]])
+    
+    export_results(results, 'results/webpage_calls_count.csv', ['website', 'casual', 'privacy', 'difference'])
+
+
+def endpoints_calls_count(cur, cur_p):
+    sql_query = "SELECT func_name, COUNT(*) as calls_count FROM javascript GROUP BY func_name ORDER BY calls_count DESC"
+    
+    cur.execute(sql_query)
+    cur_p.execute(sql_query)
+
+    endpoints_calls_count = cur.fetchall()
+    endpoints_calls_count_p = cur_p.fetchall()
+
+    endpoints_calls_count_compare = {}
+    
+    for endpoint_calls_count in endpoints_calls_count:
+        endpoints_calls_count_compare[endpoint_calls_count[0]] = []
+        endpoints_calls_count_compare[endpoint_calls_count[0]].append(endpoint_calls_count[1])
+
+    for endpoint_calls_count in endpoints_calls_count_p:
+        if endpoint_calls_count[0] in endpoints_calls_count_compare:
+            endpoints_calls_count_compare[endpoint_calls_count[0]].append(endpoint_calls_count[1])
+    
+    results = []
+    
+    for key, value in endpoints_calls_count_compare.items():
+            if len(value) == 2:
+                # We have data for both - casual and privacy crawling too.
+                results.append([key, value[0], value[1], value[0] - value[1]])
+    
+    export_results(results, 'results/endpoint_calls_count.csv', ['endpoint', 'casual', 'privacy', 'difference'])
 
 
 def analyze(cur, cur_p):
     webpage_calls_count(cur, cur_p)
+    endpoints_calls_count(cur, cur_p)
 
 
 def main():
-    db = sqlite3.connect('crawler_opensource\\crawl-data.sqlite')
-    db_p = sqlite3.connect('crawler_privacy_opensource\\crawl-data.sqlite')
+    db = sqlite3.connect('crawl_opensource\\crawl-data.sqlite')
+    db_p = sqlite3.connect('crawl_privacy_opensource\\crawl-data.sqlite')
     
     cur = db.cursor()
     cur_p = db_p.cursor()
